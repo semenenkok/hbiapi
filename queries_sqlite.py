73479266dbf80f119b12a694e0aeab5dfd019466
Konstantin <40663446+semenenkok@users.noreply.github.com>
2023-02-27 11:33:49 +0300

    added queries for RefVat and RefVarArm

--- queries_sqlite.py
+++ queries_sqlite.py
@@ -9,13 +9,50 @@ sql_GetAuditResults = """
                         , DiffRows
                         , PassedIndicator
                         , 0 as SameResultDaysCnt
-                    FROM AuditResults
-                    """
+                    FROM AuditResults                    """
+
+
+
 sql_RefAc = """ select BK_SourceMediumCode, startDate, endDate, acRate 
-                from RefAc
-            """                    
+                from RefAc    """                    
 
 sql_insert_RefAc = """
             INSERT INTO RefAc (BK_SourceMediumCode, startDate, endDate, acRate)
             VALUES ('{0}','{1}','{2}',{3}) """
 
+sql_insert_RefAc = """
+          UPDATE RefAc
+          SET acRate = {3}
+          WHERE BK_SourceMediumCode = '{0}'
+            AND startDate = '{1}' AND endDate = '{2}'  """
+
+
+
+sql_RefVat = """ select startDate, endDate, vatRate 
+                from RefVat """                    
+
+sql_insert_RefVat = """
+            INSERT RefVat (startDate, endDate, vatRate)
+            VALUES  ('{0}','{1}',{}) """
+
+sql_update_RefVat = """
+            UPDATE RefVat
+            SET vatRate = {2}
+            WHERE startDate = '{0}' AND endDate = '{1}' """
+
+
+sql_RefVatArm = """ select startDate, endDate, vatRate 
+                from RefVatArm """                    
+
+sql_insert_RefVatArm = """
+            INSERT RefVatArm (startDate, endDate, vatRate)
+            VALUES  ('{0}','{1}',{}) """
+
+sql_update_RefVatArm = """
+            UPDATE RefVatArm
+            SET vatRate = {2}
+            WHERE startDate = '{0}' AND endDate = '{1}' """
+
+
+
+            
